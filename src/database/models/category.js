d1a2fd3d2e36847b6a18cd0adfee4ae2f87cdb9d
phvleite <phvleite@gmail.com>
2022-07-16 23:39:49 -0300

    [REQ 07] realizado

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -2,9 +2,17 @@ const sequelize = require('sequelize');
 
 const Category = (sequelize, DataTypes) => {
   const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
     name: DataTypes.STRING,
   }, {
     tableName: 'Categories',
+    createdAt: false,
+    updatedAt: false,
   });
 
   return Category;
